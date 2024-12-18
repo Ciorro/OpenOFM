03ff9542a8f55252007cb69626a1601161347a6c
Ciorro <julzal2000@gmail.com>
2024-12-18 23:03:03 +0100

    Implemented media controls.

---
+++ OpenOFM.Ui/Converters/TimeSpanLessThanToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace OpenOFM.Ui.Converters
+{
+    class TimeSpanLessThanToVisibilityConverter : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new TimeSpanLessThanToVisibilityConverter();
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var maxTimeSpan = (parameter as TimeSpan?) ?? TimeSpan.FromSeconds(10);
+
+            return value is TimeSpan timeSpan && timeSpan < maxTimeSpan ?
+                Visibility.Visible :
+                Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
