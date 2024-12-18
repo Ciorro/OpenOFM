cb6808cb1ac0ca2c8516b387bcb23293da1ff36e
Ciorro <julzal2000@gmail.com>
2024-12-18 23:04:56 +0100

    Added automatic sidebar expansion based on window size.

---
+++ OpenOFM.Ui/Converters/ValueLessThanToBoolConverter.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace OpenOFM.Ui.Converters
+{
+    class ValueLessThanToBoolConverter : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new ValueLessThanToBoolConverter();
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            parameter = System.Convert.ChangeType(parameter, value.GetType(), CultureInfo.InvariantCulture);
+
+            if (value is IComparable comparable)
+            {
+                return comparable.CompareTo(parameter as IComparable) > 0;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
