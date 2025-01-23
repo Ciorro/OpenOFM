2a439c0ec18861764d20d44a93acc5efff0f2858
Ciorro <julzal2000@gmail.com>
2025-01-23 17:29:48 +0100

    Implemented favorites page.

--- OpenOFM.Ui/ViewModels/Pages/FavoritesPageViewModel.cs
+++ OpenOFM.Ui/ViewModels/Pages/FavoritesPageViewModel.cs
@@ -1,10 +1,68 @@
-﻿using OpenOFM.Ui.Navigation;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using OpenOFM.Core.Settings;
 using OpenOFM.Ui.Navigation.Attributes;
+using OpenOFM.Ui.ViewModels.Items;
+using System.ComponentModel;
+using System.Windows.Data;
 
 namespace OpenOFM.Ui.ViewModels.Pages
 {
     [PageKey("Favorites")]
-    internal class FavoritesPageViewModel : BasePageViewModel
+    internal partial class FavoritesPageViewModel : BasePageViewModel
     {
+        private readonly ISettingsProvider<Favorites> _favorites;
+
+        [ObservableProperty]
+        private ICollectionView? _favoriteSongs;
+
+        public FavoritesPageViewModel(ISettingsProvider<Favorites> favorites)
+        {
+            _favorites = favorites;
+        }
+
+        public string SongFilter
+        {
+            set
+            {
+                FavoriteSongs!.Filter = GetSongFilter(value ?? "");
+            }
+        }
+
+        public override void OnResumed()
+        {
+            var songItems = _favorites.CurrentSettings.FavoriteSongs
+                .Select(x => new SongItemViewModel(x)
+                {
+                    IsFavorite = true,
+                    IsFavoriteChanged = OnIsFavoriteChanged
+                });
+
+            FavoriteSongs = CollectionViewSource.GetDefaultView(songItems);
+        }
+
+        private void OnIsFavoriteChanged(SongItemViewModel sender, bool isFavorite)
+        {
+            if (isFavorite)
+            {
+                _favorites.CurrentSettings.FavoriteSongs.Add(sender.Song);
+            }
+            else
+            {
+                _favorites.CurrentSettings.FavoriteSongs.Remove(sender.Song);
+            }
+
+            _favorites.Save();
+        }
+
+        private Predicate<object> GetSongFilter(string filter)
+        {
+            return (obj) =>
+            {
+                var song = (obj as SongItemViewModel)!;
+
+                return song.Title.Contains(filter, StringComparison.CurrentCultureIgnoreCase) ||
+                       song.Artist.Contains(filter, StringComparison.CurrentCultureIgnoreCase);
+            };
+        }
     }
 }
